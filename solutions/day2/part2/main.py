34fa239343fede6a6d119fca1058025d54e880c2
P-Copley <pkcopley@gmail.com>
2022-12-02 13:50:47 +0000

    add missing d2p2 solution

---
+++ solutions/day2/part2/main.py
@@ -0,0 +1,15 @@
+def solution(lines):
+  choiceScores = {
+    'A X': 3,
+    'A Y': 4,
+    'A Z': 8,
+    'B X': 1,
+    'B Y': 5,
+    'B Z': 9,
+    'C X': 2,
+    'C Y': 6,
+    'C Z': 7
+  }
+
+
+  return sum(map(lambda line: choiceScores[line], lines))
