ab7fd00016aef5fb3a5e2943740fb701c47c0b16
P-Copley <pkcopley@gmail.com>
2022-12-03 16:35:52 +0000

    d3p1

--- test/test_main.py
+++ test/test_main.py
@@ -3,23 +3,20 @@ from main import solution
 
 
 def xtest_solves_simple_case():
-  testLines = [
-'A Y',
-'B X',
-'C Z',
-]
-  result = solution(testLines);
-  expected = 12
-  assert result == expected
-  print(result)
+    testLines = [
+        'vJrwpWtwJgWrhcsFMMfFFhFp',
+        'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL',
+        'PmmdzqPrVvPwwTWBwg',
+        'wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn',
+        'ttgJtRGJQctTZtZT',
+        'CrZsJsPPZsGzwwsLwLmpwMDw']
+    result = solution(testLines)
+    expected = 157
+    assert result == expected
+    print(result)
 
-def xtest_solves_example_case():
-  testLines = ['a', 'b']
-  result = solution(testLines);
-  expected = None
-  assert result == expected
 
 def test_solves_input_case():
-  testLines = lib.utils.readInput()
-  result = solution(testLines);
-  print(result)
+    testLines = lib.utils.readInput()
+    result = solution(testLines)
+    print(result)
