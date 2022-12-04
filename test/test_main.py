ef231dc63904710d1d465c3cd3f714851732a246
P-Copley <pkcopley@gmail.com>
2022-12-04 09:56:28 +0000

    d4p1

--- test/test_main.py
+++ test/test_main.py
@@ -4,14 +4,15 @@ from main import solution
 
 def xtest_solves_simple_case():
     testLines = [
-        'vJrwpWtwJgWrhcsFMMfFFhFp',
-        'jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL',
-        'PmmdzqPrVvPwwTWBwg',
-        'wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn',
-        'ttgJtRGJQctTZtZT',
-        'CrZsJsPPZsGzwwsLwLmpwMDw']
+        '2-4,6-8',
+        '2-3,4-5',
+        '5-7,7-9',
+        '2-8,3-7',
+        '6-6,4-6',
+        '2-6,4-8',
+    ]
     result = solution(testLines)
-    expected = 157
+    expected = 2
     assert result == expected
     print(result)
 
