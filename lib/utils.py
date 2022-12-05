d2c34149905eaac521891f734e453b81f62ae14e
P-Copley <pkcopley@gmail.com>
2022-12-05 10:07:15 +0000

    d5p1

--- lib/utils.py
+++ lib/utils.py
@@ -1,7 +1,8 @@
 from pathlib import Path
 
+
 def readInput():
-  filePath = Path(__file__).parent.parent.resolve()
-  fileContents = Path(f'{filePath}/input.txt').read_text()
-  lines = fileContents.split('\n')
-  return lines
+    filePath = Path(__file__).parent.parent.resolve()
+    fileContents = Path(f'{filePath}/input.txt').read_text()
+    lines = fileContents.split('\n\n')
+    return lines
