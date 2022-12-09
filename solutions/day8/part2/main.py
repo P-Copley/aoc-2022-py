647c2cc38014b5f7c7ca3817afc712a75aa1f67c
P-Copley <pkcopley@gmail.com>
2022-12-09 10:50:14 +0000

    rename helper func

--- solutions/day8/part2/main.py
+++ solutions/day8/part2/main.py
@@ -1,15 +1,14 @@
 
 
 def solution(lines):
-    visibleTrees = 0
     forest = [list(map(lambda char: int(char), list(line))) for line in lines]
 
-    def isVisible(x, y, xChange, yChange, treeHeight):
+    def countTrees(x, y, xChange, yChange, treeHeight):
         if (y < 0 or x < 0 or y >= len(forest) or x >= len(forest[0])):
             return 0
         if forest[y][x] >= treeHeight:
             return 1
-        return isVisible(x + xChange, y + yChange, xChange, yChange, treeHeight) + 1
+        return countTrees(x + xChange, y + yChange, xChange, yChange, treeHeight) + 1
 
     maxScenicScore = 0
     directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
@@ -18,10 +17,9 @@ def solution(lines):
             scenicScore = 1
             for _, direction in enumerate(directions):
                 yChange, xChange = direction
-                treeCount = isVisible(
+                treeCount = countTrees(
                     x + xChange, y + yChange, xChange, yChange, tree)
                 scenicScore *= treeCount
-                # print(tree, treeCount)
 
             if scenicScore > maxScenicScore:
                 maxScenicScore = scenicScore
