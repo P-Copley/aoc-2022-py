9e65fec51cdb118e7c17837dcff2311fb46e380f
P-Copley <pkcopley@gmail.com>
2022-12-10 21:08:06 +0000

    d9p2

--- test/test_main.py
+++ test/test_main.py
@@ -4,22 +4,22 @@ from main import solution
 
 def test_solves_simple_case():
     testLines = [
-        'R 4',
-        'U 4',
-        'L 3',
-        'D 1',
-        'R 4',
-        'D 1',
-        'L 5',
-        'R 2',
+        'R 5',
+        'U 8',
+        'L 8',
+        'D 3',
+        'R 17',
+        'D 10',
+        'L 25',
+        'U 20'
     ]
     result = solution(testLines)
-    expected = 13
+    expected = 36
     assert result == expected
     print(result)
 
 
-def xtest_solves_input_case():
+def test_solves_input_case():
     testLines = lib.utils.readInput()
     result = solution(testLines)
     print(result)
