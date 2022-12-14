a4a96849ed168369dbea5a98740c1a89057bbd14
P-Copley <pkcopley@gmail.com>
2022-12-14 09:33:52 +0000

    d13p2

--- test/test_main.py
+++ test/test_main.py
@@ -5,12 +5,12 @@ from main import solution
 def test_solves_simple_case():
     testLines = lib.utils.readInput(True)
     result = solution(testLines)
-    expected = 13
+    expected = 140
     assert result == expected
     print(result)
 
 
-def xtest_solves_input_case():
+def test_solves_input_case():
     testLines = lib.utils.readInput(False)
     result = solution(testLines)
     print(result)
